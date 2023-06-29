ea817355aca2a0910abf115adca7556fbca55a26
Alex Smith <88447828+asmith-nhsx@users.noreply.github.com>
2023-06-29 12:39:04 +0100

    Create rebase-includes.py
    Python script to actually do the rebase of the include directives in PlantUML files

---
+++ .github/workflows/rebase-includes.py
@@ -0,0 +1,30 @@
+import re
+import os
+import glob
+from pathlib import Path
+
+def replaceInFile(filepath, pattern, replace):
+    with open(filepath, 'r') as file:
+        data = file.read()
+        if (re.search(pattern, data) is not None):
+            print("matched", filepath, pattern, replace)
+            data = re.sub(pattern, "\\1" + replace + "/\\2", data)
+            with open(filepath, 'w') as file:
+                print("Replaced includes in {}".format(filepath))
+                file.write(data)
+        elif (os.path.exists(filepath + ".rep")):
+            os.remove(filepath + ".rep")
+
+def replaceInFiles(root, path, pattern, newbase):
+    rootPath = Path(root)
+    for f in glob.glob(path, recursive=True):
+        replace = newbase + str(Path(f).parent.relative_to(rootPath)).replace("\\", "/")
+        replaceInFile(f, pattern, replace)
+
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    replaceInFiles(r"/home/runner/work/data-architecture/data-architecture",
+                   r"/home/runner/work/data-architecture/data-architecture/**/*.puml",
+                   "([!]include\w{0,3}\s+?)([^<:]+?\n)",
+                   "https://raw.githubusercontent.com/co-cddo/data-architecture/main/")
